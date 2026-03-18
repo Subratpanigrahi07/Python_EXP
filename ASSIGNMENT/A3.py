7f20dc0cc36ca4c3c44e8a8dcd1b6724103dc6b0
Subrat Panigrahi <subratpanigrahi722@gmail.com>
2026-03-18 17:33:29 +0530

    Delete ASSIGNMENT directory

--- ASSIGNMENT/A3.py
+++
@@ -1,7 +0,0 @@
-#WAP TO CHECK THE LEAP YEAR
-
-x=int(input("Enter a year : "))
-if(x%4 == 0) and (x%100 != 0) or (x%400 == 0):
-    print(x,"is a leap year ")
-else:
-    print(x,"is not a leap year ")
--- ASSIGNMENT/A4.py
+++
@@ -1,8 +0,0 @@
-#WAP TO CHECK WHEATHER THE STRING IS SYMETRUCAL OR PELINDROME
-
-x=input("Enter a string : ")
-z=(str(str(x)[::-1]))
-if x == z:
-    print("it is a pelindrpme")
-else:
-    print("it is not a pelindrome")
